d972452faee03321d22c961077826a96ece36a1c
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2021-07-09 02:47:32 +0530

    add fluentbit for fun

--- test_fluent.py
+++ test_fluent.py
@@ -1,3 +1,3 @@
 from fluent import sender
-logger = sender.FluentSender('app', host='localhost', port=24224)
+logger = sender.FluentSender('app', host='localhost', port=24225)
 logger.emit('follow', {'from': 'userA', 'to': 'userB'})
